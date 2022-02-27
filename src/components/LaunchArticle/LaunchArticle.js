76539adb588f2d4524a3a7e45c5dd75b553cfbc1
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-02-27 23:33:30 +0530

    Some changes to the layout of past launches

--- src/components/LaunchArticle/LaunchArticle.js
+++ src/components/LaunchArticle/LaunchArticle.js
@@ -5,13 +5,13 @@ import './style.scss';
 
 const LaunchArticle = ({ name, link, imgUrl, description }) => (
     <div className="d-flex justify-content-center">
-        <Link to={{ pathname: link }} className="article" target="_blank" rel="noopener">
-        <div className="article__header">
-                <h5 className="m-0 p-2">{name}</h5>
-                <p className="m-0 p-2">{description}</p>
-            </div>
-            <img srcSet={`${imgUrl}`} alt={name} className="article__image" />
-        </Link>
+        <img srcSet={`${imgUrl}`} alt={name} className="article__image"/>
+            <Link to={{ pathname: link }} className="article" target="_blank" rel="noopener">
+                <div className="article__header">
+                    <h5 className="m-2 p-2">{name}</h5>
+                    <p className="m-2 p-2">{description}</p>
+                </div>
+            </Link>
     </div>
 );
 
